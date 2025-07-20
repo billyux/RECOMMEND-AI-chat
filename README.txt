80fece31e3414af7c7f7a327fcb5aa8cab223657
billyux <bongug25@gmail.com>
2025-07-20 15:34:04 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+[실행 안내]
+
+1. Python 3.9 이상 설치
+2. 아래 명령어 실행:
+   pip install -r requirements.txt
+   python main.py
---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="주식 추천 챗봇", layout="centered")
+st.title("📊 미래에셋 리포트 기반 종목 추천 챗봇")
+st.write("HyperCLOVA X와 LangChain을 기반으로 종목을 추천합니다.")
