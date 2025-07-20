a0d084c129deffd2a224764a103683dd1d6c1b1c
billyux <bongug25@gmail.com>
2025-07-20 15:52:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-
 st.set_page_config(page_title="주식 추천 챗봇", layout="centered")
 st.title(" 미래에셋 리포트 기반 종목 추천 챗봇")
 st.write("HyperCLOVA X와 LangChain을 기반으로 종목을 추천합니다.")
-import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 from langchain.text_splitter import CharacterTextSplitter
